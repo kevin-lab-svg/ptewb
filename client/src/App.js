aa99c59877b4acd63b8df01b9f0122fcf7aef230
kevin-lab-svg <la-tua-email@email.com>
2025-04-17 23:58:49 +0200

    Inizializzazione del sito personal trainer

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Benvenuto nel sito del tuo personal trainer!</h1>
+    </div>
+  );
+}
+
+export default App;
